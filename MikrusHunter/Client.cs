74fc88d6a7786c0e1ccdd3a846442d224d3cf02c
Wiktor00Nedew <wiktor.a.n.20@gmail.com>
2021-09-01 15:42:53 +0200

    Add project files.

---
+++ MikrusHunter/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace MikrusHunter
+{
+    class Client
+    {
+        public void getFile(string path)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(path, "file.txt");
+            }
+        }
+
+        public bool checkForServer()
+        {
+            var sr = new StreamReader("file.txt");
+            
+            while (sr.Peek() >= 0){
+                string text = sr.ReadLine();
+                string[] textSplited = text.Split(';');
+                List<string> textWithRemovedData = new List<string>();
+
+                textWithRemovedData.Add(textSplited[1]);
+                textWithRemovedData.Add(textSplited[3]);
+                try {
+                    if (textWithRemovedData[0] == "3.0" && int.Parse(textWithRemovedData[1].Substring(4)) > 180)
+                    {
+                        return true;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+                     
+            }
+            return false;
+        }
+    }
+}
---
+++ MikrusHunter/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MikrusHunter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NotifyIcon notify = new NotifyIcon();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Visible = false;
+            bool isServerExisting = false;
+            Client webClient = new Client();
+
+            //webClient.getFile("https://mikr.us/recykling.txt");
+
+            isServerExisting = webClient.checkForServer();
+
+            if (isServerExisting)
+            {
+                setMessageContent("Serwer dostępny", "Serwer 3.0, któremu zostało więcej niż 180 dni jest dostępny!");
+            }
+            else
+            {
+                setMessageContent("Serwer niedostępny", "Brak szukanego serwera 3.0");
+            }
+
+            notify.Visible = true;
+            notify.ShowBalloonTip(30000);
+
+            Application.Exit();
+        }
+
+        private void setMessageContent(string title, string text)
+        {
+            notify.Icon = SystemIcons.Information;
+            notify.BalloonTipTitle = title;
+            notify.BalloonTipText = text;
+            notify.BalloonTipIcon = ToolTipIcon.Info;
+        }
+    }
+}
